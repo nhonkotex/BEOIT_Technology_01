e18d37ac06e6cbe7ec0931641298c8b186c851a7
Nhơn Cao Nhật <caonhatnhon08@gmail.com>
2024-07-09 10:36:18 +0700

    first commit

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 
 
 const Home = () => {
   return (
     <div>
-        
+      <Header/>
     </div>
   )
 }
